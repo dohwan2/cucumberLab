197437017266961651bdd3fa2aecc30888e260ee
juyoungOh <ojmme625@gmail.com>
2021-01-09 22:43:46 +0900

    Qtest.qWait(700)으로 TR요청 지연 호출하여 500개 종목 일분봉 json만들기

--- kiwoom/src/bunbong.py
+++ kiwoom/src/bunbong.py
@@ -1,11 +1,11 @@
 ## JY : 일분봉 가져오는 코드
 
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtTest import *
 from kiwoom_api.api import Kiwoom, DataFeeder, Executor
 from kiwoom_api.utility import utility
 import sys
 import pandas as pd
-import time
 import datetime
 
 if __name__ == "__main__":
@@ -31,30 +31,35 @@ if __name__ == "__main__":
     stock_code = pd.read_csv('C:/Users/Administrator/PycharmProjects/kiwoom/src/codes.csv', converters={'종목코드': str})
     stock_code = stock_code[['종목코드', '기업명']]
 
+    #중간에 종료되었을 때 이미 쌓은 종목은 스킵하기 위함
+    skipcnt = 458 + 1 #021820 못쌓음
+    cnt = 0
     for index, row in stock_code.iterrows():
-        print("{} : {}".format(row['종목코드'], datetime.datetime.now()))
-        params['종목코드'] = row['종목코드']
+        cnt += 1
+        if cnt > skipcnt :
+            print("{} 번째 종목 ".format(cnt))
+            print("{} : {}".format(row['종목코드'], datetime.datetime.now()))
+            params['종목코드'] = row['종목코드']
 
-        result = {
-            'code': row['종목코드'],
-            'name': row['기업명'],
-            'data': []
-        }
+            result = {
+                'code': row['종목코드'],
+                'name': row['기업명'],
+                'data': []
+            }
 
-        data = feeder.requestPrev(trCode="OPT10080", isPrev=0, **params)
-        result['data'] += data['멀티데이터']
-
-        for i in range(24):
-            data = feeder.requestPrev(trCode="OPT10080", isPrev=2, **params)
+            data = feeder.requestPrev(trCode="OPT10080", isPrev=0, **params)
             result['data'] += data['멀티데이터']
-            print(data['멀티데이터'][0]['체결시간'])
 
-        print("총 데이터 수 : ", len(result['data']))
+            for i in range(14):
+                data = feeder.requestPrev(trCode="OPT10080", isPrev=2, **params)
+                result['data'] += data['멀티데이터']
+                print(data['멀티데이터'][0]['체결시간'])
+                QTest.qWait(700)
 
-        # json 파일로 저장
-        utility.writeJson(result, 'C:/Users/Administrator/PycharmProjects/kiwoom/result/bunbong/{}_22500.json'.format(row['종목코드']))
-        time.sleep(1)
+            print("총 데이터 수 : ", len(result['data']))
+
+            # json 파일로 저장
+            utility.writeJson(result, 'C:/Users/Administrator/PycharmProjects/kiwoom/result/bunbong/{}_13500.json'.format(row['종목코드']))
 
     print("종료합니다 {}".format(datetime.datetime.now()))
-    # df_final.to_csv('/home/ubuntu/files/{}days_before_stockprice.csv'.format(row['종목코드'], str(days)), encoding='euc-kr')
 
